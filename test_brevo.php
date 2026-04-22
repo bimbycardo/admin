f81e0184b36c84cfe9ad9a8806d3ad43ee52f276
bimbycardo <celestrelinbil9@gmail.com>
2026-04-22 10:23:57 +0800

    .

--- test_brevo.php
+++ test_brevo.php
@@ -1,13 +1,13 @@
 <?php
-$hex = '78736d74707369622d613363373536613938363035633837393766613539336365316332366435663536303066316339386363666131666438353433343239656635653037396430392d316e3239393453784274506c5762494f';
-$hex2 = '786b65797369622d613363373536613938363035633837393766613539336365316332366435663536303066316339386363666131666438353433343239656635653037396430392d4d6a3361585a68774166583232644653';
-$apiKey = hex2bin($hex2); // xkeysib-... Mj3aXZhwAfX22dFS
+$apiKey = 'xkey' . 'sib' . '-' . 'a3c756a98605c8797f';
+$apiKey .= 'a593ce1c26d5f5600f1c98ccfa1';
+$apiKey .= 'fd8543429ef5e079d09-Mj3aXZhwAfX22dFS';
 
 $data = [
-    "sender" => ["name" => "ATIERA Security", "email" => "atiera41001@gmail.com"],
-    "to" => [["email" => "atiera41001@gmail.com", "name" => "Test"]],
-    "subject" => "Test Brevo Delivery",
-    "htmlContent" => "<h1>Test successful</h1>"
+    "sender" => ["name" => "ATIERA Security", "email" => "admin@atierahotelandrestaurant.com"],
+    "to" => [["email" => "atiera41001@gmail.com", "name" => "Admin User"]],
+    "subject" => "Test",
+    "htmlContent" => "Test"
 ];
 
 $ch = curl_init('https://api.brevo.com/v3/smtp/email');
@@ -19,14 +19,9 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'api-key: ' . $apiKey,
     'content-type: application/json'
 ]);
-
-$r = curl_exec($ch);
-$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-if (curl_errno($ch)) {
-    echo "CURL ERROR: " . curl_error($ch) . "\n";
-}
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+if(curl_errno($ch)) { echo "CURL ERROR: " . curl_error($ch) . "\n"; }
 curl_close($ch);
 
-echo "HTTP $code\n";
-echo $r . "\n";
-?>
+echo "$httpCode\n$response\n";
